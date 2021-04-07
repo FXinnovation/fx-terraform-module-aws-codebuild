5a1cc0457869135674f47881385fdf76ac3f2bba
Mahidhar Reddy <Mahidhar.Reddy@fxinnovation.com>
2021-04-07 13:11:42 +0000

    feat:codebuild

--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -1,39 +1,3 @@
-variable "region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "environment_variables" {
-  type = list(object(
-    {
-      name  = string
-      value = string
-  }))
-
-  default = [
-    {
-      name  = "NO_ADDITIONAL_BUILD_VARS"
-      value = "TRUE"
-  }]
-
-  description = "A list of maps, that contain both the key 'name' and the key 'value' to be used as additional environment variables for the build"
-}
-
-variable "cache_expiration_days" {
-  type        = number
-  description = "How many days should the build cache be kept. It only works when cache_type is 'S3'"
-}
-
-variable "cache_bucket_suffix_enabled" {
-  type        = bool
-  description = "The cache bucket generates a random 13 character string to generate a unique bucket name. If set to false it uses terraform-null-label's id value"
-}
-
-variable "cache_type" {
-  type        = string
-  description = "The type of storage that will be used for the AWS CodeBuild project cache. Valid values: NO_CACHE, LOCAL, and S3.  Defaults to NO_CACHE.  If cache_type is S3, it will create an S3 bucket for storing codebuild cache inside"
-}
-
 variable "access_key" {
   description = "Credentials: AWS access key."
   type        = string
